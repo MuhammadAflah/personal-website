a0229a07201849995239a0a8c46d05b34c40f9d2
Muhammad Aflah <muhammadaflah786@gmail.com>
2023-03-25 00:44:05 +0530

    completed

---
+++ src/assets/index.js
@@ -0,0 +1,94 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";  
+import close from "./close.svg";
+import Uni_of_cali from './company/Uni_of_cali.png'
+import brototype_icon from './company/brototype_icon.jpg'
+import socialmedia from "./social-media.webp";
+import ecommerce from "./e-commerce.jpg";
+import olx from "./olx.jpg";
+import todo from "./todo.png";
+import weather from "./weather.png";
+import netflix from "./netflix.jpg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import express from "./tech/express.png"
+import sql from "./tech/sql.png"
+import aws from "./tech/aws.png"
+import firebase from "./tech/firebase.png"
+import image from "./image.jpg";
+import mail from "./mail.png";
+import call from "./call.png";
+import location from "./location.png"
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  carrent,
+  jobit,
+  tripguide,
+  Uni_of_cali,
+  brototype_icon,
+  socialmedia,
+  ecommerce,
+  olx,
+  netflix,
+  todo,
+  weather,
+  image,
+  mail,
+  call,
+  location,
+  express,
+  sql,
+  aws,
+  firebase,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,307 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    Uni_of_cali,
+    brototype_icon,
+    socialmedia,
+    ecommerce,
+    olx,
+    netflix,
+    todo,
+    weather,
+    express,
+    sql,
+    aws,
+    firebase,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "Education",
+      title: "Education",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "React JS Developer",
+      icon: web,
+    },
+    {
+      title: "Node JS Developer",
+      icon: mobile,
+    },
+    {
+      title: "MERN Stack Developer",
+      icon: backend,
+    },
+    {
+      title: "UI Designer",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "express",
+      icon: express,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+    {
+      name: "aws",
+      icon: aws,
+    },
+    
+  ];
+  
+  const experiences = [
+    {
+      title: "Junior MERN stack developer",
+      company_name: "Brototype",
+      icon: brototype_icon,
+      iconBg: "#383E56",
+      date: "July 2022 - March 2023",
+      points: [
+        "Designed and developed dynamic and responsive websites using HTML, CSS, JavaScript, React ,MongoDB, Express and Node JS. ",
+        "Worked with REST APIs to retrieve and display data from databases.",
+        "Worked with MVC frameworks to develop robust and scalable backends.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+  ];
+
+
+  const education = [
+    {
+      title: "Bachelor Of Computer Application",
+      company_name: "University Of Calicut",
+      icon: Uni_of_cali,
+      iconBg: "#383E56",
+      date: "June 2018 - March 2021",
+      points: [
+        "3 year Degree programmes affiliated to the University of Calicut, Kerala.",
+        "The programme aims to provide sound academic base from which an advanced career in Computer Application can be developed.",
+        "Conceptual grounding in computer usage as well as its practical business application will be provided.",
+        "Provide both practical and thearitical explanation of various computer technology.",
+        "Deals with different programming languages."
+    ],
+    },
+
+  ];
+  
+  
+  const projects = [
+    {
+        name: "Social Media",
+        description:
+            "A React social media application which is used to people to interact with each other. Its frontend is build with react and backend is connect with Nodejs.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongoose",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Nodejs",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "ExpressJs",
+                color: "red-text-gradient",
+            },
+            {
+                name: "MUI",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "SASS",
+                color: "green-text-gradient",
+            },
+        ],
+        image: socialmedia,
+        source_code_link: "https://github.com/MuhammadAflah/Social_media.git",
+    },
+    {
+        name: "E-Commerce",
+        description:
+            "An e-commerce bag store which consist of customized admin panel to manage products, orders, sales report, etc. All the users and products are managed by admin.",
+        tags: [
+            {
+                name: "Nodejs",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "Mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "ExpressJs",
+                color: "pink-text-gradient",
+            },
+            {
+                name: "HBS",
+                color: "red-text-gradient",
+            },
+        ],
+        image: ecommerce,
+        source_code_link: "https://github.com/MuhammadAflah/e-commerce.git",
+    },
+    {
+        name: "Netflix Clone",
+        description:
+            "Use TMDB API for fetching movie details.",
+        tags: [
+            {
+                name: "React",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "JavaScript",
+                color: "green-text-gradient",
+            },
+            
+        ],
+        image: netflix,
+        source_code_link: "https://github.com/MuhammadAflah/Netflix-clone-react.git",
+    },
+    {
+        name: "OLX Clone",
+        description:
+            "User and admin authentication using jwt and store user details and images in firebase.",
+        tags: [
+            {
+                name: "React",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "JavaScript",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Firebase",
+                color: "pink-text-gradient",
+            },
+            
+        ],
+        image: olx,
+        source_code_link: "https://github.com/MuhammadAflah/OLX-with-react-and-nodejs.git",
+    },
+    {
+        name: "ToDo App",
+        description:
+            "User can list their daily tasks and it also shows the completed and pending tasks.",
+        tags: [
+            {
+                name: "React",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "JavaScript",
+                color: "green-text-gradient",
+            },
+            
+        ],
+        image: todo,
+        source_code_link: "https://github.com/MuhammadAflah/To-Do-using-react.git",
+    },
+    {
+        name: "Weather App",
+        description:
+            "Live weather forcast using Openweathermap API.",
+        tags: [
+            {
+                name: "React",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "JavaScript",
+                color: "green-text-gradient",
+            },
+            
+        ],
+        image: weather,
+        source_code_link: "https://github.com/MuhammadAflah/Weather_app.git",
+    },
+];
+  
+  export { services, technologies, experiences, projects ,education};
